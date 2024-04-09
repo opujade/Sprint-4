a442d38fa9e1efe7654b452c2a978bffd4c4e417
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-04-09 11:35:00 +0200

    Exercici 1

--- buld/index.js
+++ buld/index.js
@@ -1,16 +1,31 @@
-"use strict";
-const dadjokeContainer = document.getElementById('dadjoke');
-function getDadJoke() {
-    const options = {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-        },
-    };
-    fetch('https://icanhazdadjoke.com/', options)
-        .then((res) => res.json())
-        .then((data) => printDadJoke(data));
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const dadJokeContainer = document.getElementById('dad-joke-container');
+const nextDadJokeBtn = document.getElementById('next-dad-joke-btn');
+nextDadJokeBtn.addEventListener('click', () => fetchDadJoke());
+function fetchDadJoke() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch('https://icanhazdadjoke.com/', {
+            headers: {
+                Accept: 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}`);
+        }
+        const data = yield response.json();
+        printDadJoke(data);
+    });
 }
 function printDadJoke(data) {
-    dadjokeContainer.innerHTML = data.joke;
+    dadJokeContainer.innerHTML = data.joke;
 }
+fetchDadJoke();
+export {};
---
+++ ts/index.ts
@@ -0,0 +1,30 @@
+import { dadJoke } from './interface/interface';
+
+const dadJokeContainer = document.getElementById(
+  'dad-joke-container'
+) as HTMLDivElement;
+const nextDadJokeBtn = document.getElementById(
+  'next-dad-joke-btn'
+) as HTMLButtonElement;
+nextDadJokeBtn.addEventListener('click', () => fetchDadJoke());
+
+async function fetchDadJoke(): Promise<void> {
+  const response = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error ${response.status}`);
+  }
+
+  const data: dadJoke = await response.json();
+  printDadJoke(data);
+}
+
+function printDadJoke(data: dadJoke): void {
+  dadJokeContainer.innerHTML = data.joke;
+}
+
+fetchDadJoke();
